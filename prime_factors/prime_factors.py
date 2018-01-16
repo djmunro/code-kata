6e64ab624ad1ea3e037c6d316fc221d38f9e390a
Dave Munro <dave.munro@harman.com>
2018-01-16 10:19:11 -0500

    finished prime factors kata

--- prime_factors/prime_factors.py
+++ prime_factors/prime_factors.py
@@ -1,4 +1,11 @@
 class PrimeFactors(object):
     @staticmethod
     def generate(number):
-        return []
+        primes = []
+        candidate = 2
+        while number > 1:
+            while number % candidate == 0:
+                primes.append(candidate)
+                number /= candidate
+            candidate += 1
+        return primes
--- prime_factors/prime_factors_test.py
+++ prime_factors/prime_factors_test.py
@@ -4,8 +4,29 @@ from prime_factors.prime_factors import PrimeFactors
 
 
 class PrimeFactorsTest(unittest.TestCase):
+    def list(self, *args):
+        list = []
+        for i in args:
+            list.append(i)
+        return list
+
     def test_one(self):
         self.assertEqual(self.list(), PrimeFactors.generate(1))
 
-    def list(self):
-        return []
+    def test_two(self):
+        self.assertEqual(self.list(2), PrimeFactors.generate(2))
+
+    def test_three(self):
+        self.assertEqual(self.list(3), PrimeFactors.generate(3))
+
+    def test_four(self):
+        self.assertEqual(self.list(2, 2), PrimeFactors.generate(4))
+
+    def test_six(self):
+        self.assertEqual(self.list(2, 3), PrimeFactors.generate(6))
+
+    def test_eight(self):
+        self.assertEqual(self.list(2, 2, 2), PrimeFactors.generate(8))
+
+    def test_nine(self):
+        self.assertEqual(self.list(3, 3), PrimeFactors.generate(9))
